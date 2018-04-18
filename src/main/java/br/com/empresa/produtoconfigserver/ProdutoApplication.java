39b6e9fc3979480e3136c07bef52520b7fdd495e
Ramon Amorim <ramon.neogrid@neogrid.com>
2018-04-18 10:56:35 -0300

    initial commit

---
+++ src/main/java/br/com/empresa/produtoconfigserver/ProdutoApplication.java
@@ -0,0 +1,14 @@
+package br.com.empresa.produtoconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoApplication.class, args);
+	}
+}
